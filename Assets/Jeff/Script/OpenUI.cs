6f80a543b8ba8b10b84b4c790363742dc79396b4
JeffTao1 <jefft18ca@gmail.com>
2026-01-29 17:26:20 -0500

    UI set up

---
+++ Assets/Jeff/Script/OpenUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class OpenUI : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject setting;
+    public UnityEvent PausedAll;
+    public UnityEvent ResumAll;
+    bool isPaused;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PressedToPlay();
+        }
+    }
+    public void PressedToPlay()
+    {
+        menu.SetActive(!menu.activeSelf);
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            PausedAll.Invoke();
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            ResumAll.Invoke();
+        }
+    }
+}
