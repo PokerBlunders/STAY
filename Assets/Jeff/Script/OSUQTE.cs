35f69406964f586ba0b24690a1d0422719b16309
JeffTao1 <jefft18ca@gmail.com>
2026-02-08 17:18:34 -0500

    osu qte done

---
+++ Assets/Jeff/Script/OSUQTE.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OSUQTE : MonoBehaviour
+{
+    public GameObject circlePrefab;
+    public RectTransform canvasRoot;
+
+    public int maxPerGroup = 4;
+
+    public float spawnInterval = 1f;   
+    public float circleLifeTime = 0.5f; 
+
+    private int spawnedCount = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (spawnedCount < maxPerGroup)
+        {
+            SpawnCircle();
+
+            spawnedCount++;
+
+         
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
+        Debug.Log("good");
+    }
+
+    void SpawnCircle()
+    {
+        
+        Vector2 randomPos = new Vector2(
+            Random.Range(-300, 300),
+            Random.Range(-200, 200)
+        );
+
+        
+        GameObject circle = Instantiate(circlePrefab, canvasRoot);
+        circle.GetComponent<RectTransform>().anchoredPosition = randomPos;
+
+        // if hit 
+        circle.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Debug.Log("Hit!");
+            Destroy(circle);
+        });
+
+        //gone in 1s
+        Destroy(circle, circleLifeTime);
+    }
+}
